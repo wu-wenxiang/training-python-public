2538143c09beff58b33546ff734e978e9f359798
Wu Wenxiang <wuwenxiang.sh@gmail.com>
2018-08-18 06:45:17 +0800

    update algo

--- src/algorithm/sortAlgo.py
+++ src/algorithm/sortAlgo.py
@@ -97,6 +97,69 @@ def merge_sort(alist):
             k=k+1
 #     print("Merging ",alist)
 
+def shell_sort(alist):
+    sublistcount = len(alist)//2
+    while sublistcount > 0:
+        for startposition in range(sublistcount):
+            gapInsertionSort(alist,startposition,sublistcount)
+#             print("After increments of size", sublistcount,
+#                   "The list is", alist)
+        sublistcount = sublistcount // 2
+
+def gapInsertionSort(alist,start,gap):
+    for i in range(start+gap,len(alist),gap):
+        currentvalue = alist[i]
+        position = i
+        while position>=gap and alist[position-gap]>currentvalue:
+            alist[position]=alist[position-gap]
+            position = position-gap
+        alist[position]=currentvalue
+
+# 堆构建
+def build_heap(arr):
+    length = len(arr)
+    # 找到最后一个非叶子节点
+    index = length//2 - 1
+    # 从最后一个非节点开始依次向上做堆调整
+    for i in range(index, -1, -1):
+        heapify(arr, i)
+
+# 堆调整，这里按大顶堆进行调整
+def heapify(arr, index):
+    # 叶子节点无须再做堆调整
+    if index > len(arr)/2 - 1:
+        return None
+    max_index = index
+    left_child_index = index*2 + 1
+    right_child_index = index*2 + 2
+    if left_child_index < len(arr) and arr[left_child_index] > arr[max_index]:
+        max_index = left_child_index
+    if right_child_index < len(arr) and arr[right_child_index] > arr[max_index]:
+        max_index = right_child_index
+
+    if arr[max_index] != arr[index]:
+        temp = arr[max_index]
+        arr[max_index] = arr[index]
+        arr[index] = temp
+        # 递归调整
+        heapify(arr, max_index)
+
+# 堆排序
+def heap_sort(arr):
+    arr_sorted = []
+    # 先构建堆
+    build_heap(arr)
+    # 交换堆顶元素和堆末尾元素，交换后堆末尾元素为最大值元素
+    while len(arr) != 0:
+        temp = arr[0]
+        arr[0] = arr[len(arr)-1]
+        arr[len(arr)-1] = temp
+        arr_sorted.insert(0, arr.pop(len(arr)-1))
+        # 调整
+        heapify(arr, 0)
+    arr[:] = []
+    arr.extend(arr_sorted)
+
 if __name__ == '__main__':
     iterObj = [1, 9, 7, 3, 2, 0, 81, 9, 32, 2, 6]
     exp = [0, 1, 2, 2, 3, 6, 7, 9, 9, 32, 81]
