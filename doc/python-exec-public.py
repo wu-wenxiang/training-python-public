97a14ba0c6cc7baf0eea2826803e59461a029e3f
Wu Wenxiang <wuwenxiang.sh@gmail.com>
2018-09-25 23:58:35 +0800

    add xlsxwriter

--- doc/python-exec-public.py
+++ doc/python-exec-public.py
@@ -2155,6 +2155,7 @@ print(s & IPSet([IP('10.0.0.0/8')]))
 '''
 Tip_110103 dnspython
 '''
+
 # http://www.dnspython.org/examples.html
 # https://github.com/rthalley/dnspython
 
@@ -2262,6 +2263,42 @@ import dns.e164
 n = dns.e164.from_e164("+1 555 1212")
 print(n)
 print(dns.e164.to_e164(n))
+
+'''
+Tip_110104 xlsxwriter
+'''
+# https://github.com/jmcnamara/XlsxWriter
+
+import xlsxwriter
+
+# Create an new Excel file and add a worksheet.
+workbook = xlsxwriter.Workbook('demo.xlsx')
+worksheet = workbook.add_worksheet()
+
+# Widen the first column to make the text clearer.
+worksheet.set_column('A:A', 20)
+
+# Add a bold format to use to highlight cells.
+bold = workbook.add_format({'bold': True})
+
+# Write some simple text.
+worksheet.write('A1', 'Hello')
+
+# Text with formatting.
+worksheet.write('A2', 'World', bold)
+
+# Write some numbers, with row/column notation.
+worksheet.write(2, 0, 123)
+worksheet.write(3, 0, 123.456)
+
+# Insert an image.
+# worksheet.insert_image('B5', 'logo.png')
+
+workbook.close()
+
+'''
+Tip_110105 
+'''
 
 '''
 Tip_110201 如何使用Fabric实现自动化部署？
